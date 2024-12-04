25c293537facbdd6cef42402eb0c9d57f5453d5e
BugHunter-X <askzenai@gmail.com>
2024-12-04 21:54:09 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
